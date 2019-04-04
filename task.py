9d6667e5e5a9915927a4bb1b3810f4900677c0b9
Dawid Gwiżdż <dawcio.gwizdz@gmail.com>
2019-04-04 16:34:30 +0200

    First fix

--- task.py
+++ task.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 class Car(object):
 	def __init__(self):
 		self.speed = 0
 		self.wheel_angle = 0
-	def act(*event):
-		eName = event[1]
-		eValue = event[2]
-		eTime = event[3]
+	def act(self,*event):
+		eName = event[0]
+		eValue = float(event[1])
+		eTime = float(event[2])
 
 		print(eName,eValue,eTime)
 
@@ -17,22 +19,23 @@ class Car(object):
 				self.wheel_angle=360+180
 			if(self.wheel_angle < -360-180):
 				self.wheel_angle=-360-180
-	def wheel_angle():
+'''
+	def wheel_angle(self):
 		return self.wheel_angle
-	def speed():
+	def speed(self):
 		return self.speed
-
+'''
 print("Hello!")
 instruction="1. Add acceleration\n2. Change angle\n3. Exit"
 
 car1 = Car()
 while True:
-	print("Current angle: %f, Current speed %f") % (car1.wheel_angle, car1.speed)
+	print("Current angle: "+str(car1.wheel_angle)+", Current speed: "+ str(car1.speed))
 	print(instruction)
-	option = raw_input()
+	option = input()
 	if option == '3':
 		break
 	else:
-		value = raw_input("Type value:\n")
-		time = raw_input("Type duration time:\n")
+		value = input("Type value:\n")
+		time = input("Type duration time:\n")
 		car1.act(option,value,time)
