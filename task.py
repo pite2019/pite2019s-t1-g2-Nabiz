fc970bee338a3dd3d34e416e739c842111ce0acd
Dawid Gwiżdż <dawcio.gwizdz@gmail.com>
2019-04-11 17:38:33 +0200

    Upgrade1

--- task.py
+++ task.py
@@ -1,41 +1,105 @@
 #!/usr/bin/python3
 
-class Car(object):
+class Event():
+
+	names = {1:'startEngine',
+	2:'stopEngine',
+	3:'accelerate',
+	4:'slow',
+	5:'turnLeft',
+	6:'turnRight'}
+
+	def __init__(self,option):
+		self.name = Event.names[option]
+		self.value = 0
+		self.time = 0
+		if option > 2: 
+			self.value = abs(float(input('Insert value:')))
+		if  2 < option < 5:
+			self.time = abs(float(input('Insert duration:')))
+
+class Car():
+
 	def __init__(self):
+		self.engine_status = False
 		self.speed = 0
 		self.wheel_angle = 0
-	def act(self,*event):
-		eName = event[0]
-		eValue = float(event[1])
-		eTime = float(event[2])
+			
+	def getParams(self):
+		return (self.engine_status, self.speed, self.wheel_angle)
 
-		print(eName,eValue,eTime)
+	def act(self,event):
+		n = event.name
+		v= event.value
+		t = event.time
 
-		if eName == '1':
-			self.speed += eValue * eTime 
-		elif eName == '2':
-			self.wheel_angle += eValue * eTime;
-			if(self.wheel_angle > 360+180):
-				self.wheel_angle=360+180
-			if(self.wheel_angle < -360-180):
-				self.wheel_angle=-360-180
-'''
-	def wheel_angle(self):
-		return self.wheel_angle
-	def speed(self):
-		return self.speed
-'''
-print("Hello!")
-instruction="1. Add acceleration\n2. Change angle\n3. Exit"
+		if n == 'startEngine':
+			self.startEngine()
+		elif n == 'stopEngine':
+			self.stopEngine()
+		elif n == 'accelerate':
+			self.accelerate(v,t)
+		elif n == 'slow':
+			self.slow(v,t)
+		elif n == 'turnLeft':
+			self.turnLeft(v)
+		elif n == 'turnRight':
+			self.turnRight(v)
+		else:
+			pass
 
-car1 = Car()
-while True:
-	print("Current angle: "+str(car1.wheel_angle)+", Current speed: "+ str(car1.speed))
-	print(instruction)
-	option = input()
-	if option == '3':
-		break
-	else:
-		value = input("Type value:\n")
-		time = input("Type duration time:\n")
-		car1.act(option,value,time)
+	def startEngine(self):
+		self.engine_status = True
+
+	def stopEngine(self):
+		self.engine_status = False
+		self.speed = 0
+
+	def accelerate(self,value=0,time=0):
+		if self.engine_status == True:
+			self.speed += value * time
+	
+	def slow(self,value=0,time=0):
+		self.speed -= value * time
+		if self.speed < 0:
+			self.speed = 0
+
+	def turnLeft(self,value=0):
+		self.wheel_angle -= value
+		if self.wheel_angle < -720:
+			self.wheel_angle = -720
+	
+	def turnRight(self,value=0):
+		self.wheel_angle += value
+		if self.wheel_angle > 720:
+			self.wheel_angle = 720
+
+
+
+def main():
+
+	car = Car()
+	while True:
+		print('''MENU:
+1. Start engine
+2. Stop engine
+3. Add acceleration
+4. Slow
+5. Turn Left
+6. Turn Right
+0. Exit''')
+		try:
+			option = int(input())
+			if option == 0:
+				break
+			else:
+				event = Event(option)
+		except Exception as e:
+			print("Your input is wrong")
+		
+		car.act(event)
+		print("Engine status: {} Speed: {} Wheel angle: {}".\
+				format(*car.getParams()))
+
+if __name__ == "__main__":
+	main()
